47f7d28deeb863683cd17eb26633f99fcaa46608
Ritom Gupta <ritomgupta99@gmail.com>
2024-02-02 00:15:31 -0800

    add routing

--- README.md
+++ README.md
@@ -28,3 +28,7 @@ npm init @open-wc
 For most of the tools, the configuration is in the `package.json` to reduce the amount of files in your project.
 
 If you customize the configuration a lot, you can consider moving them to individual files.
+
+## Routing
+
+https://blog.bitsrc.io/routing-with-litelement-2a29465ec778
---
+++ src/pages/gallery-view.ts
@@ -0,0 +1,107 @@
+import { LitElement, html, css } from "lit";
+import { property, customElement } from 'lit/decorators.js';
+
+const logo = new URL('../../../assets/bg-3.png', import.meta.url).href;
+
+@customElement('gallery-view')
+export class Gallery extends LitElement {
+    @property({ type: String }) header = 'gallery';
+
+    static styles = css`
+    :host {
+      min-height: 100svh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      font-family:'Helvetica','Arial',sans-serif;
+      font-size: calc(10px + 3vmin);
+      color: #ffffff;
+      margin: 0 auto;
+      text-align: left;
+      user-select:none;
+    }
+
+    main {
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: calc(15vh);
+      margin-bottom: auto;
+      font-size: calc(10px + 3.1vmin);
+
+    }
+
+    .logo {
+      margin-top: 12px;
+      animation: app-logo-spin infinite 10s linear;
+      font-size: calc(10px + 8vmin);
+    }
+
+    @keyframes app-logo-spin {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+
+    .button-row {
+      text-align: left;
+      align-items: center;
+    }
+
+    .button-row a {
+      background-color: #ffffff44; /* Green background */
+      color: white; /* White text */
+      padding: 5px 15px; /* Padding around the text */
+      text-align: center; /* Center the text */
+      text-decoration: none; /* No underline */
+      font-size: calc(10px + 1vmin); /* Set font size */
+      cursor: pointer; /* Cursor to pointer on hover */
+      border: none; /* No border */
+      border-radius: 10px; /* Rounded corners */
+    }
+
+    .app-footer {
+      font-size: calc(12px + 0.5vmin);
+      align-items: center;
+    }
+
+    .app-footer a {
+      margin-left: 0px;
+      text-decoration:none;
+      color: white;
+    }
+
+  `;
+
+    render() {
+        return html`
+      <style>
+        :host {
+          background-color: var(--index-sp-background-color, #3b3458);
+          background: rgba(0, 0, 0, 0.45) url(${logo}) no-repeat center center
+            fixed;
+          background-size: cover;
+          background-blend-mode: darken;
+        }
+      </style>
+      <main>
+        <div class="logo">🚀</div>
+        <h1 style="display:table-caption;">${this.header}</h1>
+        <div class="button-row">
+          <a href="https://share.sideprojekts.com/submit" target="_blank"
+            >Submit my projekt</a
+          >
+          <a style="background-color: #00000066;" href="https://share.sideprojekts.com/x-twitter" target="_blank"
+            >𝕏</a
+          >
+        </div>
+      </main>
+
+      <p class="app-footer">
+        built by <a href="https://go.rightonrittman.in/twitter" target="_blank">@rittmang</a> ✌🏼
+      </p>
+    `;
+    }
+}
